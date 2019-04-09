387bf7f929c5c063cd7b04b5d93962e0affbe71b
lexdewilligen <lexdewilligen@outlook.com>
2019-04-09 22:59:25 +0200

    implemented loadMigrationsFrom

--- src/CommentableServiceProvider.php
+++ src/CommentableServiceProvider.php
@@ -14,9 +14,7 @@ class CommentableServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../database/migrations/create_comments_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_comments_table.php'),
-        ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/create_comments_table.php');
 
         $this->publishes([
             __DIR__.'/../config/commentable.php' => config_path('commentable.php'),
